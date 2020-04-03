9fd5e757840e87a1ff1bda80c6a67181729da492
bondalex-maker <bondarenko.alex.sergeevich@gmail.com>
2020-04-03 13:29:35 +0300

    Remove changeRoute from Registration and Login pages

--- client/src/pages/RegistrationPage/RegistrationPage.js
+++ client/src/pages/RegistrationPage/RegistrationPage.js
@@ -11,9 +11,6 @@ import CONSTANTS from '../../constants';
 const RegistrationPage = (props) => {
   props.clearError();
 
-  const changeRoute = () => {
-    props.history.replace('/');
-  };
 
   return (
     <div className={ styles.signUpPage }>
@@ -25,7 +22,7 @@ const RegistrationPage = (props) => {
                   style={ {textDecoration: 'none'} }><span>Login</span></Link>
           </div>
         </div>
-        <RegistrationForm changeRoute={ changeRoute }/>
+        <RegistrationForm />
       </div>
       <div className={ styles.footer }>
         <div className={ styles.articlesMainContainer }>
