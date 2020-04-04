36a907fb070630ba27d3e868df2b4d5c2a283850
bondalex-maker <bondarenko.alex.sergeevich@gmail.com>
2020-04-04 19:16:33 +0300

    Refactor: RegistrationPage RegistrationForm usage

--- client/src/pages/RegistrationPage/RegistrationPage.js
+++ client/src/pages/RegistrationPage/RegistrationPage.js
@@ -1,44 +1,74 @@
 import React from 'react';
 import Logo from '../../components/Logo';
 import RegistrationForm
-  from '../../components/RegistrationForm/RegistrationForm';
+    from '../../components/RegistrationForm/RegistrationForm';
 import styles from './RegistrationPage.module.sass';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { clearErrorSignUpAndLogin } from '../../actions/actionCreator';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {authActionRegister, clearAuth, clearErrorSignUpAndLogin} from '../../actions/actionCreator';
 import CONSTANTS from '../../constants';
 import Articles from "../../components/Articles/Articles";
+import Error from "../../components/Error/Error";
+import _ from 'lodash'
 
 const RegistrationPage = (props) => {
-  props.clearError();
+    const {initialValues, authClear, error, signUpUser} = props;
+    const handleSubmit = (values) => {
+        signUpUser(_.pick(values, [
+            'firstName',
+            'lastName',
+            'displayName',
+            'email',
+            'password',
+            'role',
+        ]));
+    };
 
 
-  return (
-    <div className={ styles.signUpPage }>
-      <div className={ styles.signUpContainer }>
-        <div className={ styles.headerSignUpPage }>
-          <Logo src={ `${ CONSTANTS.STATIC_IMAGES_PATH }logo.png` }/>
-          <div className={ styles.linkLoginContainer }>
-            <Link to='/login'
-                  style={ {textDecoration: 'none'} }><span>Login</span></Link>
-          </div>
+    return (
+        <div className={styles.signUpPage}>
+            <div className={styles.signUpContainer}>
+                <div className={styles.headerSignUpPage}>
+                    <Logo src={`${CONSTANTS.STATIC_IMAGES_PATH}logo.png`}/>
+                    <div className={styles.linkLoginContainer}>
+                        <Link to='/login'
+                              style={{textDecoration: 'none'}}><span>Login</span></Link>
+                    </div>
+                </div>
+                <div className={styles.signUpFormContainer}>
+                    {error && <Error data={error.data} status={error.status}
+                                     clearError={authClear}/>}
+                    <div className={styles.headerFormContainer}>
+                        <h2>
+                            CREATE AN ACCOUNT
+                        </h2>
+                        <h4>
+                            We always keep your name and email address private.
+                        </h4>
+                    </div>
+
+                    <RegistrationForm onSubmit={handleSubmit} initialValues={{
+                        role: CONSTANTS.CUSTOMER,
+                    }}/>
+                </div>
+            </div>
+            <div className={styles.footer}>
+                <div className={styles.articlesMainContainer}>
+                    <Articles/>
+
+                </div>
+            </div>
         </div>
-        <RegistrationForm />
-      </div>
-      <div className={ styles.footer }>
-        <div className={ styles.articlesMainContainer }>
-           <Articles/>
-
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    clearError: () => dispatch(clearErrorSignUpAndLogin()),
-  };
-};
+const mapStateToProps = state => state.auth;
 
-export default connect(null, mapDispatchToProps)(RegistrationPage);
+const mapDispatchToProps = dispatch => ({
+        clearError: () => dispatch(clearErrorSignUpAndLogin()),
+        authClear: () => dispatch(clearAuth()),
+        signUpUser: data => dispatch(authActionRegister(data)),
+
+    });
+
+export default connect(mapStateToProps, mapDispatchToProps)(RegistrationPage);
